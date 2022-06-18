a88ec2790a15af91de619fbe51c30e651655747d
Pavel Sobolev <paveloom@riseup.net>
2022-06-18 17:53:39 +0300

    Add a header to the CSV file.

--- src/model/fun/write_to.rs
+++ src/model/fun/write_to.rs
@@ -4,13 +4,28 @@ use crate::model::io::output;
 use crate::model::Model;
 
 use std::fs::{create_dir_all, File};
-use std::io::BufWriter;
+use std::io::{BufWriter, Write};
 use std::path::Path;
 
 use anyhow::{Context, Result};
 use bincode::Options;
+use indoc::indoc;
 use num::Float;
 use serde::Serialize;
+
+/// Header of the `coords.dat` file
+const COORDS_CSV_HEADER: &str = indoc! {"
+    # Galactic coordinates of the objects in the Cartesian system
+    #
+    # Descriptions:
+    #
+    # name: Name of the object
+    # x: X coordinate [kpc] [f64]
+    # y: Y coordinate [kpc] [f64]
+    # z: Z coordinate [kpc] [f64]
+    # obj_type: Type of the object
+    #\n
+"};
 
 impl<F: Float> Model<F> {
     /// Write the model data to files inside the directory
@@ -35,15 +50,20 @@ impl<F: Float> Model<F> {
         // Define paths to the text and the binary files
         let dat_path = &dat_dir.join("coords.dat");
         let bin_path = &bin_dir.join("coords.bin");
-        // Create a writer for the text file
+        // Open files for writing
+        let mut dat_file = File::create(&dat_path)
+            .with_context(|| "Couldn't open the file {dat_path:?} in write-only mode")?;
+        let bin_file = File::create(&bin_path)
+            .with_context(|| "Couldn't open the file {bin_path:?} in write-only mode")?;
+        // Write the header to the text file
+        write!(&mut dat_file, "{}", COORDS_CSV_HEADER)
+            .with_context(|| "Couldn't write the header to {dat_path:?}")?;
+        // Create a CSV writer for the text file
         let mut dat_wtr = csv::WriterBuilder::default()
             .delimiter(b' ')
-            .from_path(dat_path)
-            .with_context(|| format!("Couldn't write to the file {dat_path:?}"))?;
-        // Create a writer for the binary file
-        let mut bin_wtr = BufWriter::new(
-            File::create(&bin_path).with_context(|| "Couldn't write to the file {bin_path:?}")?,
-        );
+            .from_writer(dat_file);
+        // Create a `bincode` writer for the binary file
+        let mut bin_wtr = BufWriter::new(bin_file);
         // Create an options struct for `bincode`
         let bin_options = bincode::DefaultOptions::default()
             .with_little_endian()
@@ -53,7 +73,7 @@ impl<F: Float> Model<F> {
         // Write the records in the binary format
         bin_options
             .serialize_into(&mut bin_wtr, &records)
-            .with_context(|| format!("Couldn't write a record to {bin_path:?}"))?;
+            .with_context(|| format!("Couldn't write records to {bin_path:?}"))?;
         // For each record
         for record in records {
             // Write it in the text format
