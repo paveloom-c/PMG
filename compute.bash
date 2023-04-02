d9e911dce5376385f9aa48f623e259f9c89478f7
Pavel Sobolev <paveloom@riseup.net>
2023-04-02 22:22:25 +0300

    Change `center.dat`:
    - Rename it to `near_the_solar_circle.dat`, change the description.
    - Leave only HMSFRs in the data.
    - Perform the `objects` goal for this data set, too.

--- compute.bash
+++ compute.bash
@@ -16,14 +16,15 @@ echo -e "\n${PAD}Step 2. Perform computations"
 
 echo -e "\n${PAD}> Compute per-object data..."
 cargo run -r -- -o data/output/all --goal objects -i data/input/all.dat &>/dev/null
+cargo run -r -- -o data/output/near_the_solar_circle --goal objects -i data/input/near_the_solar_circle.dat &>/dev/null
 cargo run -r -- -o data/output/hmsfrs --goal objects -i data/input/hmsfrs.dat &>/dev/null
 cargo run -r -- -o data/output/hmsfrs_test --goal objects -i data/input/hmsfrs.dat \
   --u-sun 11 --theta-sun 255 --r-0 8.34 &>/dev/null
 
 echo -e "${PAD}> Fit all..."
 cargo run -r -- -o data/output/all --goal fit -i data/input/all.dat &>/dev/null
-echo -e "${PAD}> Fit center..."
-cargo run -r -- -o data/output/center --goal fit -i data/input/center.dat &>/dev/null
+echo -e "${PAD}> Fit near the solar circle..."
+cargo run -r -- -o data/output/near_the_solar_circle --goal fit -i data/input/near_the_solar_circle.dat &>/dev/null
 echo -e "${PAD}> Fit HMSFRs..."
 cargo run -r -- -o data/output/hmsfrs --goal fit -i data/input/hmsfrs.dat &>/dev/null
 
@@ -37,18 +38,22 @@ ${PAD}        and the catalogue from Reid et al. (2019)"
 echo -e "${PAD}Step 4. Plot projections in each plane"
 
 echo -e "\n${PAD}All by type:"
-./julia.bash scripts/coords.jl -o "'All by type'" data/output/all/
+./julia.bash scripts/coords.jl -o "'All by type'" data/output/all
 echo -e "${PAD}All by source:"
-./julia.bash scripts/coords.jl -s -o "'All by source'" data/output/all/
+./julia.bash scripts/coords.jl -s -o "'All by source'" data/output/all
+echo -e "${PAD}Near the solar circle:"
+./julia.bash scripts/coords.jl -s -o "'Near the solar circle'" data/output/near_the_solar_circle
 echo -e "${PAD}HMSFRs:"
-./julia.bash scripts/coords.jl -s -o "HMSFRs" data/output/hmsfrs/
+./julia.bash scripts/coords.jl -s -o "HMSFRs" data/output/hmsfrs
 
 echo "${PAD}Step 5. Plot the rotation curve"
 
 echo -e "\n${PAD}All by type:"
-./julia.bash scripts/rotcurve.jl -o "'All by type'" data/output/all/
+./julia.bash scripts/rotcurve.jl -o "'All by type'" data/output/all
 echo -e "${PAD}All by source:"
-./julia.bash scripts/rotcurve.jl -s -o "'All by source'" data/output/all/
+./julia.bash scripts/rotcurve.jl -s -o "'All by source'" data/output/all
+echo -e "${PAD}Near the solar circle:"
+./julia.bash scripts/rotcurve.jl -s -o "'Near the solar circle'" data/output/near_the_solar_circle
 echo -e "${PAD}HMSFRs:"
 ./julia.bash scripts/rotcurve.jl -s -o "HMSFRs" data/output/hmsfrs/
 echo -e "${PAD}HMSFRs (test):"
